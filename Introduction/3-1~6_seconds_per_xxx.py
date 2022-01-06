8675fa5e7e66d233a1f61a3dc3493a0b2f2e24cb
june <78254849+haderade@users.noreply.github.com>
2022-01-06 23:05:41 +0900

    Create 3-1~6_seconds_per_xxx.py

---
+++ Introduction/3-1~6_seconds_per_xxx.py
@@ -0,0 +1,11 @@
+#入門python3の復習問題 P62
+
+seconds_per_hour = 60 * 60
+print("一時間あたりの秒数：" + str(seconds_per_hour))
+
+seconds_per_day = seconds_per_hour * 24
+print("一日あたりの秒数：" + str(seconds_per_day))
+
+print("一日あたりの秒数 / 一時間あたりの秒数：" + str(seconds_per_day/seconds_per_hour))
+
+print("一日あたりの秒数 // 一時間あたりの秒数：" + str(seconds_per_day//seconds_per_hour))
