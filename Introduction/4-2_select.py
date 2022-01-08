f7193efac2d66e2cded7319f355528979c861e2c
june <78254849+haderade@users.noreply.github.com>
2022-01-08 23:00:20 +0900

    Add files via upload

---
+++ Introduction/4-2_select.py
@@ -0,0 +1,11 @@
+#smallとgreenの条件にあった食べ物を表示
+
+small = True #適宜変更
+green = False #適宜変更
+
+if small == False and green == True:
+    print("watermelon")
+elif small == True and green == False:
+    print("cherry")
+else :
+    print("pea")
