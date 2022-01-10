16a865ef4f133ad47cb4b457f5b7ba52cce9962c
june <78254849+haderade@users.noreply.github.com>
2022-01-10 16:39:50 +0900

    Add files via upload

---
+++ Introduction/5-2_QA.py
@@ -0,0 +1,15 @@
+#質問と回答の表示
+
+questions = [
+    "We don't serve strings around here. Are you a string.",
+    "What is said on Father's Day in the forest?",
+    "What makes the sound 'Sis! Boom! Bah!'?"
+]
+answers = [
+    "An exploding sheep.",
+    "No, I'm a frayed knot.",
+    "'Pop!' goes thw weasel."
+]
+
+print("Q:",questions[0],questions[1],questions[2],sep='\n')
+print("A:",answers[0],answers[1],answers[2],sep='\n')
