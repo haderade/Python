ee6ba4a98bc0d148d288e8ef896bc38935e3acf8
june <78254849+haderade@users.noreply.github.com>
2022-01-11 20:16:11 +0900

    Add files via upload

---
+++ Introduction/5-3_locate.py
@@ -0,0 +1,6 @@
+thing1 = "roast beef"
+thing2 = "ham"
+thing3 = "head"
+thing4 = "clam"
+
+print("My kitty cat likes %s," % thing1,"My kitty cat likes %s," % thing2,"My kitty cat fell on his %s," % thing3,"And now thinks he's a %s," % thing4,sep='\n',)
