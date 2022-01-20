f178765e7b13fba699467df20185228a86c26d01
june <78254849+haderade@users.noreply.github.com>
2022-01-20 21:56:00 +0900

    Add files via upload

--- study/log_analytics.py
+++ study/log_analytics.py
@@ -1,3 +1,4 @@
+from doctest import OutputChecker
 import json
 import datetime
 import pathlib
@@ -5,13 +6,17 @@ from tarfile import PAX_NAME_FIELDS
 
 file_name = [] #ファイル名を書くのするリストを作成。
 
-p_file = pathlib.Path('log')
-file = p_file.glob('*.log') #ファイル名を取得
-
-print(p_file)
-print(file)
+file = pathlib.Path('log').glob('*.log') #ファイル名を取得
 
 for p in file:
     file_name.append(p.name) #ファイル名をリストに格納
 
 print(file_name)
+
+file_numder = len(file_name) #ファイル数を取得
+file_output = open("output.txt","w") #output.txtファイルを作成
+
+#json_open = open("log/" + file_name[0],"r") #jsonファイルオープン
+#json_load = json.load(json_open) #jsonファイル読み込み
+
+#print(json_load)
